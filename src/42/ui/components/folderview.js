2ea45b9f7fd455fa8f29dc213d266f40ada9beb6
zo <zombectro@gmail.com>
2022-06-19 01:03:34 +0200

    Add folderview.js

---
+++ src/42/ui/components/folderview.js
@@ -0,0 +1,64 @@
+import "./icon.js"
+import Component from "../class/Component.js"
+import dispatch from "../../fabric/dom/dispatch.js"
+import disk from "../../system/fs/disk.js"
+// import selectable from "../trait/selectable.js"
+// import listen from "../../fabric/dom/listen.js"
+
+export class FolderView extends Component {
+  static definition = {
+    tag: "ui-folderview",
+
+    tabIndex: -1,
+
+    props: {
+      path: {
+        type: "string",
+        reflect: true,
+        default: "/",
+      },
+      glob: {
+        type: "boolean",
+        fromView: true,
+      },
+      selection: {
+        type: "array",
+        default: [],
+      },
+      // items: {
+      //   type: "array",
+      //   default: [],
+      //   computed: "{{path|getItems}}",
+      // },
+    },
+
+    computed: {
+      items: "{{path|getItems}}",
+    },
+
+    content: {
+      scope: "items",
+      repeat: {
+        tag: "ui-icon",
+        path: "{{.}}",
+        aria: { selected: "{{../../selection|includes(.)}}" },
+      },
+    },
+  }
+
+  getItems(path) {
+    let dir
+    try {
+      dir = this.glob
+        ? disk.glob(path.endsWith("*") ? path : path + "*")
+        : disk.readDir(path, { absolute: true })
+    } catch (error) {
+      dir = []
+      dispatch(this, error)
+    }
+
+    return dir
+  }
+}
+
+export default Component.define(FolderView)
