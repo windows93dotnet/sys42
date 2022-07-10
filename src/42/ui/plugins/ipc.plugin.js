f5af5839b6f7fbf66727f7c361c3e5f1ba07982b
zo <zombectro@gmail.com>
2022-07-10 12:36:19 +0200

    Improve ipc.plugin.js

--- src/42/ui/plugins/ipc.plugin.js
+++ src/42/ui/plugins/ipc.plugin.js
@@ -9,8 +9,14 @@ const DEFAULTS = {
   downstream: true,
 }
 
+function getData(queue, ctx) {
+  const data = {}
+  for (const key of queue) allocate(data, key, ctx.reactive.get(key), "/")
+  return data
+}
+
 export default async (ctx, options) => {
-  const { upstream } = configure(DEFAULTS, options)
+  const { upstream, downstream } = configure(DEFAULTS, options)
 
   if (upstream) {
     if (inTop && ctx.parentId) {
@@ -23,10 +29,32 @@ export default async (ctx, options) => {
       const bus = ipc.to(globalThis.top)
 
       ctx.reactive.on("update", (queue) => {
-        const data = {}
-        for (const key of queue) allocate(data, key, ctx.reactive.get(key), "/")
-        bus.send(`42-ui-ipc-${ctx.id}`, data)
+        bus.send(`42-ui-ipc-${ctx.id}`, getData(queue, ctx))
       })
+    }
+  }
+
+  if (downstream) {
+    if (inTop) {
+      const senders = []
+
+      ipc.on("42-ui-ipc-handshake", (data, meta) => {
+        senders.push(meta.send)
+      })
+
+      ctx.reactive.on("update", (queue) => {
+        const data = getData(queue, ctx)
+        for (const send of senders) send(`42-ui-ipc-${ctx.id}`, data)
+      })
+    }
+
+    if (inIframe) {
+      ipc
+        .to(globalThis.top)
+        .on(`42-ui-ipc-${ctx.parentId}`, (data) => {
+          ctx.reactive.assign("/", data)
+        })
+        .send("42-ui-ipc-handshake")
     }
   }
 }
