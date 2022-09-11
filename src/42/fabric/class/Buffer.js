6112d066df48d0479169fafd1822b87dd2d8b5c6
zo <zombectro@gmail.com>
2022-09-11 10:19:58 +0200

    Improve Buffer.js

--- src/42/fabric/class/Buffer.js
+++ src/42/fabric/class/Buffer.js
@@ -1,44 +1,77 @@
-const dataViewKeys = []
+// @read https://github.com/tc39/proposal-resizablearraybuffer
+// @read https://chromestatus.com/feature/4668361878274048
 
-for (const key of Reflect.ownKeys(DataView.prototype)) {
-  if (typeof key !== "string") continue
-  if (key.startsWith("get") || key.startsWith("set")) {
-    dataViewKeys.push(key)
-  }
-}
-
-export default class Buffer extends Uint8Array {
+export default class Buffer {
   #memory
   #view
+  #arr
   #length
 
   constructor() {
-    const memory = new WebAssembly.Memory({ initial: 1 })
-    super(memory.buffer)
-    this.#memory = memory
-    this.#view = new Uint8Array(this.#memory.buffer)
+    this.#memory = new WebAssembly.Memory({ initial: 1 })
+    this.#arr = new Uint8Array(this.#memory.buffer)
     this.#length = 0
+    this.offset = 0
   }
 
   get length() {
     return this.#length
   }
-  get byteLength() {
-    return this.#length
+  set length(n) {
+    if (n > this.#length) this.#length = n
+    if (n > this.#memory.buffer.byteLength) {
+      const remain = n - this.#memory.buffer.byteLength
+      this.#memory.grow(Math.ceil(remain / 0x01_00_00))
+      this.#arr = new Uint8Array(this.#memory.buffer)
+      this.#view = undefined
+    }
+  }
+  get buffer() {
+    return this.#memory.buffer
   }
   get value() {
-    return this.#view.slice(0, this.#length).buffer
+    return this.#memory.buffer.slice(0, this.#length)
+  }
+  get view() {
+    this.#view ??= new DataView(this.#memory.buffer)
+    return this.#view
+  }
+
+  at(n) {
+    return n < 0 ? this.#arr.at(this.#length + n) : this.#arr.at(n)
   }
 
   write(chunk, offset = this.#length) {
     if (typeof chunk === "string") chunk = new TextEncoder().encode(chunk)
+    this.length = offset + chunk.byteLength
+    this.#arr.set(chunk, offset)
+  }
 
-    if (offset + chunk.byteLength > this.#memory.buffer.byteLength) {
-      this.#memory.grow(Math.ceil(chunk.byteLength / 0x01_00_00))
-      this.#view = new Uint8Array(this.#memory.buffer)
+  read(length, offset = this.offset, encoding = "utf8") {
+    const end = length === undefined ? this.length : offset + length
+    const buffer = this.#memory.buffer.slice(offset, end)
+    this.offset = end
+    return encoding ? new TextDecoder(encoding).decode(buffer) : buffer
+  }
+}
+
+const p = Buffer.prototype
+
+for (const getKey of Reflect.ownKeys(DataView.prototype)) {
+  if (typeof getKey === "string" && getKey.startsWith("get")) {
+    const key = getKey.slice(3)
+    const BPE = globalThis[`${key}Array`].BYTES_PER_ELEMENT
+    const setKey = `set${key}`
+
+    p[`read${key}`] = function (offset = this.offset) {
+      this.offset = offset + BPE
+      return this.view[getKey](offset)
     }
 
-    this.#view.set(chunk, offset)
-    this.#length += chunk.byteLength
+    p[`write${key}`] = function (value, offset = this.length, littleEndian) {
+      this.length = offset + BPE
+      this.view[setKey](offset, value, littleEndian)
+      return BPE
+    }
   }
 }
--- src/tests/42/fabric/class/Buffer.test.js
+++ src/tests/42/fabric/class/Buffer.test.js
@@ -9,36 +9,55 @@ test("Buffer", (t) => {
   const buf = new Buffer()
 
   t.is(buf.length, 0)
-  t.is(buf.byteLength, 0)
 
-  t.is(buf[0], 0)
-  t.is(buf[1], 0)
-  t.is(buf[999], 0)
-  t.is(buf[0x00_ff_ff], 0)
+  t.is(buf.at(-1), 0)
+  t.is(buf.at(0), 0)
+  t.is(buf.at(1), 0)
+  t.is(buf.at(999), 0)
+  t.is(buf.at(0x00_ff_ff), 0)
 
   // outside of first memory page
-  t.is(buf[0x01_00_00], undefined)
+  t.is(buf.at(0x01_00_00), undefined)
+
+  buf.write("hello")
+  t.is(String.fromCharCode(buf.at(0)), "h")
+  t.is(String.fromCharCode(buf.at(-1)), "o")
 })
 
 test("Buffer.write", (t) => {
   const buf = new Buffer()
-
   t.eq(buf.value, new ArrayBuffer())
 
   buf.write("hello")
-
   t.eq(buf.value, stringToBuffer("hello"))
 
   buf.write(" world")
-
   t.eq(buf.value, stringToBuffer("hello world"))
 
   buf.write(stringToBuffer("!!!"))
-
   t.eq(buf.value, stringToBuffer("hello world!!!"))
+
+  buf.write(stringToBuffer("yo yo"), 0)
+  t.eq(buf.value, stringToBuffer("yo yo world!!!"))
+
+  buf.write(stringToBuffer("lorem ipsum dolor"), 0)
+  t.eq(buf.value, stringToBuffer("lorem ipsum dolor"))
 })
 
-test("Buffer.write", "page 2", (t) => {
+test("Buffer.read", (t) => {
+  const buf = new Buffer()
+  buf.write("hello world")
+  t.is(buf.read(), "hello world")
+  buf.offset = 0
+
+  t.is(buf.read(1), "h")
+  t.is(buf.read(1), "e")
+  t.is(buf.read(2, 0), "he")
+  t.is(buf.read(2), "ll")
+  t.is(buf.read(), "o world")
+})
+
+test("Buffer.write", "auto grow", (t) => {
   const buf = new Buffer()
 
   t.eq(buf.value, new ArrayBuffer())
@@ -51,16 +70,32 @@ test("Buffer.write", "page 2", (t) => {
   t.eq(buf.value, data.buffer)
 })
 
-// test("DataView prototype", (t) => {
-//   const buf = new Buffer()
+test.only("Buffer.writeInt16", (t) => {
+  t.eq(
+    new Uint8Array([0, 42, 0, 9]).buffer,
+    new Uint8Array([0, 42, 0, 42]).buffer
+  )
+})
 
-//   buf.setInt16(1, 42)
-//   t.is(buf.getInt16(1), 42)
+test("Buffer.writeInt16", (t) => {
+  const buf = new Buffer()
 
-//   t.eq(buf.buffer)
+  buf.writeInt16(42)
+  t.is(buf.view.getInt16(0), 42)
+  t.is(buf.readInt16(), 42)
 
-//   // buf.setBigInt64(0, 3n)
-//   // t.is(buf.getBigInt64(0), 3n)
+  t.eq(buf.value, new Uint8Array([0, 42]).buffer)
 
-//   // t.eq(buf.dataView.buffer)
-// })
+  buf.writeInt16(12)
+  t.eq(buf.value, new Uint8Array([0, 42, 0, 12]).buffer)
+})
+
+test("Buffer.writeBigInt64", (t) => {
+  const buf = new Buffer()
+
+  buf.writeBigInt64(3n)
+  t.is(buf.view.getBigInt64(0), 3n)
+  t.is(buf.readBigInt64(), 3n)
+
+  t.eq(buf.value, new Uint8Array([0, 0, 0, 0, 0, 0, 0, 0x03]).buffer)
+})
