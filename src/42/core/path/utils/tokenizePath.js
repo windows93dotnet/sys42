c762b8c2492af33cc5f66701f3fe0b134b0749b8
zo <zombectro@gmail.com>
2022-09-26 17:47:06 +0200

    Fix tokenizePath.js

--- src/42/core/path/utils/tokenizePath.js
+++ src/42/core/path/utils/tokenizePath.js
@@ -1,5 +1,29 @@
-import trim from "../../../fabric/type/string/trim.js"
+export default function tokenizePath(source) {
+  const tokens = []
+  let buffer = ""
+  let current = 0
 
-export default function tokenizePath(path) {
-  return trim(path, "/").split("/")
+  const flush = () => {
+    if (buffer) {
+      tokens.push(buffer)
+      buffer = ""
+    }
+  }
+
+  while (current < source.length) {
+    const char = source[current]
+
+    if (char === "/") {
+      flush()
+      current++
+      continue
+    }
+
+    buffer += char
+    current++
+  }
+
+  flush()
+
+  return tokens
 }
