f66ebb9b01e8e76f01cee6f17b125f518a3e8f92
zo <zombectro@gmail.com>
2022-09-26 15:39:39 +0200

    Fix control with scope and value

--- src/42/ui/register.js
+++ src/42/ui/register.js
@@ -1,5 +1,16 @@
 import arrify from "../fabric/type/any/arrify.js"
 
+export function registerRenderer(ctx, scope, renderer) {
+  ctx.renderers[scope] ??= new Set()
+  ctx.renderers[scope].add(renderer)
+  ctx.cancel.signal.addEventListener("abort", () => {
+    if (ctx.renderers[scope]) {
+      ctx.renderers[scope].delete(renderer)
+      if (ctx.renderers[scope].size === 0) delete ctx.renderers[scope]
+    }
+  })
+}
+
 export default function register(ctx, loc, fn) {
   let scopes
   let renderer // TODO: check renderer garbage collection
@@ -21,15 +32,10 @@ export default function register(ctx, loc, fn) {
   }
 
   for (const scope of scopes) {
-    ctx.renderers[scope] ??= new Set()
-    ctx.renderers[scope].add(renderer)
-    ctx.cancel.signal.addEventListener("abort", () => {
-      if (ctx.renderers[scope]) {
-        ctx.renderers[scope].delete(renderer)
-        if (ctx.renderers[scope].size === 0) delete ctx.renderers[scope]
-      }
-    })
+    registerRenderer(ctx, scope, renderer)
   }
 
   renderer()
 }
+
+register.registerRenderer = registerRenderer
