c5b9f7f7d59a3aa576ca51c0aaaca5906a2a39b0
zo <zombectro@gmail.com>
2022-10-09 19:44:08 +0200

    Add sandbox + sw experiments

--- src/42.sw.js
+++ src/42.sw.js
@@ -1,4 +1,7 @@
+// @read https://bugs.chromium.org/p/chromium/issues/detail?id=468227#c15
+
 import getDriver from "./42/core/fs/getDriver.js"
+import parseFilename from "./42/core/path/parseFilename.js"
 import Disk from "./42/core/fs/Disk.js"
 
 const disk = new Disk()
@@ -13,26 +16,66 @@ self.addEventListener("activate", () => {
 })
 
 self.addEventListener("fetch", (e) => {
-  // console.log("fetch", e.request.url)
-  const url = new URL(e.request.url)
-  const { id, mask } = disk.getIdAndMask(url.pathname)
+  const { pathname } = new URL(e.request.url)
+  // console.log(pathname)
+
+  if (pathname === "/vhost.html") {
+    const res = new Response(
+      `
+<link rel="stylesheet" href="/test.css">
+yo vhost!!
+<script type="module">
+console.log(111, location)
+console.log(112, window.top.document.body.append("evil"))
+</script>
+
+      `,
+      {
+        headers: {
+          // "Origin": "null",
+          "Content-Type": "text/html",
+          "Content-Security-Policy": "sandbox allow-scripts;",
+          // "sandbox allow-scripts allow-same-origin; default-src https://localhost:4200 'unsafe-inline' data:;",
+          // "Cross-Origin-Resource-Policy": "same-origin",
+          // "Cross-Origin-Embedder-Policy:": "require-corp",
+          // "Cross-Origin-Opener-Policy:": "same-origin",
+        },
+      }
+    )
+    e.respondWith(res)
+    return
+  }
+
+  const { id, mask } = pathname.endsWith("/")
+    ? disk.getIdAndMask(pathname + "index.html")
+    : disk.getIdAndMask(pathname)
+
   if (mask !== 0 && typeof id === "number") {
-    console.log(`🛰 ${url.pathname}: id:${id} mask:${mask}`)
+    const obj = parseFilename(pathname, { headers: true })
+
+    console.group(`🛰 ${pathname}: id:${id} mask:${mask}`)
+    console.log(e.request)
+    console.log(obj)
+    console.groupEnd()
+
     e.respondWith(
       getDriver(mask)
-        .then((driver) => driver.open(url.pathname))
-        .then((blob) => new Response(blob))
-    )
+        .then((driver) => driver.open(pathname))
+        .then(
+          (blob) =>
+            new Response(blob, {
+              headers: {
+                ...obj.headers,
+                "Content-Security-Policy": "sandbox allow-scripts;",
+                // "sandbox allow-scripts allow-same-origin; default-src https://localhost:4200 'unsafe-inline' data:;",
 
-    // @read https://bugs.chromium.org/p/chromium/issues/detail?id=468227#c15
-    // e.respondWith(
-    //   new Response("Hello world", {
-    //     headers: [
-    //       ["content-type", "application/octet-stream; charset=utf-8"],
-    //       ["Content-Disposition", "attachment; filename=test.txt"],
-    //     ],
-    //   })
-    // )
+                // "Cross-Origin-Resource-Policy": "same-origin",
+                // "Cross-Origin-Embedder-Policy:": "require-corp",
+                // "Cross-Origin-Opener-Policy:": "same-origin",
+              },
+            })
+        )
+    )
   }
 })
 
@@ -40,7 +83,6 @@ for (const event of [
   "activate",
   "controllerchange",
   "error",
-  // "fetch",
   "install",
   "message",
   "statechange",
