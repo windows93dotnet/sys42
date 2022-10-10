9f12f841adf0ab50f4946b76bab2e4c05b9a7082
zo <zombectro@gmail.com>
2022-10-10 14:55:19 +0200

    Add vhost service worker

---
+++ src/vhost.sw.js
@@ -0,0 +1,55 @@
+import ipc from "./42/core/ipc.js"
+import parseFilename from "./42/core/path/parseFilename.js"
+
+const LIST = new Set(["/test.html", "/test.css"])
+
+self.addEventListener("install", () => {
+  // Activate worker immediately
+  self.skipWaiting()
+})
+
+self.addEventListener("activate", () => {
+  // Become available to all pages
+  self.clients.claim()
+})
+
+self.addEventListener("fetch", (e) => {
+  const { pathname } = new URL(e.request.url)
+
+  if (
+    LIST.has(pathname)
+    // e.request.referrer.startsWith(location.origin) ||
+    // (e.request.destination === "iframe" &&
+    //   !e.request.url.endsWith("/vhost.html"))
+  ) {
+    const obj = parseFilename(pathname, { headers: true })
+
+    e.respondWith(
+      self.clients
+        .matchAll()
+        .then((clients) => {
+          for (const client of clients) {
+            if (client.url.startsWith(location.origin + "/vhost.html")) {
+              return client
+            }
+          }
+        })
+        .then((client) => ipc.to(client).send("42_REQUEST_URL", pathname))
+        .then((args) => new Response(args, { headers: obj.headers }))
+    )
+  }
+})
+
+for (const event of [
+  "activate",
+  "controllerchange",
+  "error",
+  "install",
+  "message",
+  "statechange",
+  "updatefound",
+]) {
+  self.addEventListener(event, () => {
+    console.log(`🫴 ${event}`)
+  })
+}
