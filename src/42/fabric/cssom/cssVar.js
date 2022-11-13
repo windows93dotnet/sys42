eabbad3d63adb06543253bc7617c642be7953b89
zo <zombectro@gmail.com>
2022-11-13 22:29:36 +0100

    Add cssVar.js

---
+++ src/42/fabric/cssom/cssVar.js
@@ -0,0 +1,26 @@
+export function setCssVar(el, name, val) {
+  if (val === undefined) {
+    val = el
+    el = document.documentElement
+  }
+
+  if (!name.startsWith("--")) name = "--" + name
+
+  el.style.setProperty(name, val)
+}
+
+export function getCssVar(el, name) {
+  if (name === undefined) {
+    name = el
+    el = document.documentElement
+  }
+
+  if (!name.startsWith("--")) name = "--" + name
+
+  return getComputedStyle(el).getPropertyValue(name)
+}
+
+export default {
+  get: getCssVar,
+  set: setCssVar,
+}
