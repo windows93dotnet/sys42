8bbddfd319eb6e43d1fc6b6033873a31c612de1d
zo <zombectro@gmail.com>
2022-11-18 12:37:07 +0100

    Add ini.js

---
+++ src/42/core/formats/ini.js
@@ -0,0 +1,180 @@
+import locate from "../../fabric/locator/locate.js"
+import allocate from "../../fabric/locator/allocate.js"
+
+export function tokenize(source) {
+  const tokens = []
+
+  let type = "key"
+  let buffer = ""
+  let current = 0
+
+  const eatWhitespace = () => {
+    let i = current
+    let n = source.charCodeAt(i)
+
+    while (n === 32 /* space */ && i < source.length - 1) {
+      n = source.charCodeAt(++i)
+    }
+
+    current = i
+  }
+
+  const flush = () => {
+    if (buffer) {
+      tokens.push({ type, buffer })
+      buffer = ""
+    } else if (type === "value") {
+      tokens.push({ type, buffer: undefined })
+      buffer = ""
+    }
+  }
+
+  let lastCharEscaped = false
+  let isNewline = true
+
+  while (current < source.length) {
+    const char = source[current]
+
+    if (isNewline) {
+      if (char === ";") {
+        type = "comment"
+        current++
+        eatWhitespace()
+        continue
+      }
+
+      if (char === "[") {
+        type = "object"
+        current++
+        continue
+      }
+
+      isNewline = false
+    }
+
+    if (lastCharEscaped) {
+      lastCharEscaped = false
+      buffer += char
+      current++
+      continue
+    }
+
+    if (char === "\\") {
+      lastCharEscaped = true
+      current++
+      continue
+    }
+
+    if (char === "[" && source[current + 1] === "]") {
+      type = "array"
+      flush()
+      type = "key"
+      current += 2
+      eatWhitespace()
+      continue
+    }
+
+    if (type === "key" && char === " ") {
+      current++
+      continue
+    }
+
+    if (char === "]") {
+      flush()
+      type = "key"
+      current++
+      continue
+    }
+
+    if (char === "=") {
+      flush()
+      type = "value"
+      current++
+      eatWhitespace()
+      continue
+    }
+
+    if (char === "\r") {
+      current++
+      continue
+    }
+
+    if (char === "\n") {
+      isNewline = true
+      flush()
+      type = "key"
+      current++
+      continue
+    }
+
+    buffer += char
+    current++
+  }
+
+  flush()
+
+  return tokens
+}
+
+const sep = "."
+const opt = { hashmap: true }
+
+export function decode(str, options) {
+  const parse = options?.parse ?? JSON.parse
+  const out = Object.create(null)
+
+  let key
+  let array
+  let current = out
+
+  for (const { type, buffer } of tokenize(str)) {
+    if (key || array) {
+      if (type === "value") {
+        let val
+        try {
+          val = parse(buffer)
+        } catch {
+          val = buffer
+        }
+
+        if (array) array.push(val)
+        else {
+          allocate(current, key, val, sep, opt)
+          key = undefined
+        }
+
+        continue
+      } else if (key) {
+        allocate(current, key, true, sep, opt)
+      }
+    }
+
+    if (type === "key") {
+      if (array) array = undefined
+      key = buffer
+      continue
+    }
+
+    if (type === "object") {
+      current = Object.create(null)
+      allocate(out, buffer, current, sep, opt)
+      continue
+    }
+
+    if (type === "array") {
+      array = locate(current, buffer) ?? []
+      if (!Array.isArray(array)) array = [array]
+      allocate(current, buffer, array, sep, opt)
+      continue
+    }
+  }
+
+  if (key) {
+    allocate(current, key, true, sep, opt)
+  }
+
+  return out
+}
+
+export const ini = { decode }
+export default ini
---
+++ src/tests/42/core/formats/ini.test.js
@@ -0,0 +1,140 @@
+/* eslint-disable no-proto */
+import ini, { tokenize } from "../../../../42/core/formats/ini.js"
+import test from "../../../../42/test.js"
+
+// example from https://github.com/npm/ini#usage
+const example = `\
+; this comment is being ignored
+scope = global
+
+[database]
+user = dbuser
+password = dbpassword
+database = use_this_database
+
+[paths.default]
+datadir = /var/lib/data
+arr[] = first value
+arr[] = second value
+arr[] = third value
+lastkey = 42
+`
+
+const object = {
+  scope: "global",
+  database: {
+    user: "dbuser",
+    password: "dbpassword",
+    database: "use_this_database",
+  },
+  paths: {
+    default: {
+      datadir: "/var/lib/data",
+      arr: ["first value", "second value", "third value"],
+      lastkey: 42,
+    },
+  },
+}
+
+test("tokenize", (t) => {
+  t.eq(tokenize(example), [
+    { type: "comment", buffer: "this comment is being ignored" },
+    { type: "key", buffer: "scope" },
+    { type: "value", buffer: "global" },
+    { type: "object", buffer: "database" },
+    { type: "key", buffer: "user" },
+    { type: "value", buffer: "dbuser" },
+    { type: "key", buffer: "password" },
+    { type: "value", buffer: "dbpassword" },
+    { type: "key", buffer: "database" },
+    { type: "value", buffer: "use_this_database" },
+    { type: "object", buffer: "paths.default" },
+    { type: "key", buffer: "datadir" },
+    { type: "value", buffer: "/var/lib/data" },
+    { type: "array", buffer: "arr" },
+    { type: "value", buffer: "first value" },
+    { type: "array", buffer: "arr" },
+    { type: "value", buffer: "second value" },
+    { type: "array", buffer: "arr" },
+    { type: "value", buffer: "third value" },
+    { type: "key", buffer: "lastkey" },
+    { type: "value", buffer: "42" },
+  ])
+})
+
+test("decode", (t) => {
+  t.alike(ini.decode(example), object)
+})
+
+// @src https://github.com/npm/ini/blob/main/test
+
+test.tasks(
+  [
+    { str: "foo", obj: { foo: true } },
+    { str: "foo\nbar=false", obj: { foo: true, bar: false } },
+    { str: "foo\nbar=", obj: { foo: true, bar: undefined } },
+    { str: "foo\nbar=\nbaz", obj: { foo: true, bar: undefined, baz: true } },
+    {
+      str: "foo\nbar=\nbaz=",
+      obj: { foo: true, bar: undefined, baz: undefined },
+    },
+    { str: "[foo]\nbar", obj: { foo: { bar: true } } },
+    { str: "[x]\ny=1\nz=2", obj: { x: { y: 1, z: 2 } } },
+    { str: "[x]\r\ny=1\r\nz=2", obj: { x: { y: 1, z: 2 } } },
+    { str: "[x]\r\ny=1\r\ny[]=2\r\n", obj: { x: { y: [1, 2] } } },
+    { str: "=just junk!\r\n[foo]\r\nbar\r\n", obj: { foo: { bar: true } } },
+  ],
+
+  (test, { str, obj, tokens }) => {
+    test(str, (t) => {
+      if (tokens) t.eq(tokenize(str), tokens)
+      if (obj) t.alike(ini.decode(str), obj)
+    })
+  }
+)
+
+test("proto", (t) => {
+  const res = ini.decode(`
+__proto__ = quux
+constructor.prototype.foo = asdfasdf
+foo = baz
+[__proto__]
+foo = bar
+[other]
+foo = asdf
+[kid.__proto__.foo]
+foo = kid
+[arrproto]
+hello = snyk
+__proto__[] = you did a good job
+__proto__[] = so you deserve arrays
+thanks = true
+[ctor.constructor.prototype]
+foo = asdfasdf
+`)
+
+  t.isUndefined(res.__proto__)
+  t.isUndefined(res.kid.__proto__)
+  t.isUndefined(res.kid.foo.__proto__)
+  t.isUndefined(res.arrproto.__proto__)
+  t.isUndefined(Object.prototype.foo)
+  t.isUndefined(Object.prototype[0])
+  t.isUndefined(Object.prototype["0"])
+  t.isUndefined(Object.prototype[1])
+  t.isUndefined(Object.prototype["1"])
+  t.isUndefined(Array.prototype[0])
+  t.isUndefined(Array.prototype[1])
+
+  t.alike(res, {
+    constructor: { prototype: { foo: "asdfasdf" } },
+    foo: "baz",
+    other: { foo: "asdf" },
+    kid: { foo: { foo: "kid" } },
+    arrproto: { hello: "snyk", thanks: true },
+    ctor: { constructor: { prototype: { foo: "asdfasdf" } } },
+  })
+})
+
+// test("encode", (t) => {
+//   t.alike(ini.encode(example), object)
+// })
