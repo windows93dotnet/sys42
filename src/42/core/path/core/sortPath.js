6da6e512c839a42be44d3d089d80716a0bdd1a7d
zo <zombectro@gmail.com>
2022-12-02 15:55:50 +0100

    Improve sortPath.js

--- src/42/core/path/core/sortPath.js
+++ src/42/core/path/core/sortPath.js
@@ -1,30 +1,42 @@
-export default function sortPath(paths, options) {
-  const splitted = paths.map((path) => ({
-    path,
-    segments: (options?.key === undefined ? path : path[options?.key])
-      .split("/")
-      .map((item, i, arr) => (i < arr.length - 1 ? `${item}/` : item))
-      .filter(Boolean), // TODO: use flatMap
-  }))
+import getPathInfos from "../getPathInfos.js"
+import locate from "../../../fabric/locator/locate.js"
 
-  if (options?.stem) {
-    splitted.sort(({ segments: a }, { segments: b }) => {
-      const aStem = a.at(-1)
-      const bStem = b.at(-1)
-      const aIsDir = aStem.endsWith("/")
-      const bIsDir = bStem.endsWith("/")
-      if (aIsDir === bIsDir) return aStem.localeCompare(bStem)
-      if (aIsDir === false) return 1
-      if (bIsDir === false) return -1
-      return 0
+export default function sortPath(paths, options) {
+  const type =
+    options?.type ?? (typeof options === "string" ? options : undefined)
+
+  if (type) {
+    const splitted = paths.map((path) => ({
+      path,
+      infos: getPathInfos(
+        options?.key === undefined ? path : path[options.key]
+      ),
+    }))
+
+    splitted.sort(({ infos: a }, { infos: b }) => {
+      if (a.isDir === b.isDir) {
+        const res = locate(a, type).localeCompare(locate(b, type))
+        if (res === 0) return a.stem.localeCompare(b.stem)
+        return res
+      }
+
+      return a.isDir === false ? 1 : b.isDir === false ? -1 : 0
     })
 
     return splitted.map(({ path }) => path)
   }
 
+  const splitted = paths.map((path) => ({
+    path,
+    segments: (options?.key === undefined ? path : path[options.key])
+      .split("/")
+      .map((item, i, arr) => (i < arr.length - 1 ? `${item}/` : item))
+      .filter(Boolean), // TODO: use flatMap
+  }))
+
   splitted.sort(({ segments: a }, { segments: b }) => {
-    const len = Math.max(a.length, b.length)
-    for (let i = 0; i < len; i++) {
+    const l = Math.max(a.length, b.length)
+    for (let i = 0; i < l; i++) {
       if (i in a === false) return -1
       if (i in b === false) return 1
       const c = a[i].localeCompare(b[i])
