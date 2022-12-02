6b03c66186cc1800c7216b5a449e7242791b4bf1
zo <zombectro@gmail.com>
2022-12-02 02:19:21 +0100

    Add grid focus management

--- src/42/ui/components/grid.js
+++ src/42/ui/components/grid.js
@@ -1,4 +1,7 @@
 import Component from "../classes/Component.js"
+import debounce from "../../fabric/type/function/debounce.js"
+
+const { indexOf } = Array.prototype
 
 export class Grid extends Component {
   static definition = {
@@ -7,6 +10,19 @@ export class Grid extends Component {
 
     aria: {
       multiselectable: "{{multiselectable}}",
+    },
+
+    traits: {
+      selectable: {
+        items: ":scope ui-icon",
+        dragger: { ignore: "ui-icon" },
+        init() {
+          return this.selection
+        },
+        key({ path }) {
+          return path
+        },
+      },
     },
 
     props: {
@@ -25,11 +41,69 @@ export class Grid extends Component {
       content: {
         type: "array",
         default: [],
-        update() {
-          // console.log("update")
+        update(initial) {
+          if (!initial) {
+            this.#refreshIconPerLine()
+          }
         },
       },
     },
+
+    on: {
+      repeatable: true,
+      prevent: true,
+      ArrowUp: "{{moveFocusUp()}}",
+      ArrowDown: "{{moveFocusDown()}}",
+      ArrowLeft: "{{moveFocusLeft()}}",
+      ArrowRight: "{{moveFocusRight()}}",
+    },
+  }
+
+  moveFocusUp() {
+    const index = indexOf.call(this.items, document.activeElement)
+    this.items[index === -1 ? 0 : index - this.iconsPerLine]?.focus()
+  }
+
+  moveFocusDown() {
+    const index = indexOf.call(this.items, document.activeElement)
+    this.items[index === -1 ? 0 : index + this.iconsPerLine]?.focus()
+  }
+
+  moveFocusLeft() {
+    const index = indexOf.call(this.items, document.activeElement)
+    this.items[index === -1 ? 0 : index - 1]?.focus()
+  }
+
+  moveFocusRight() {
+    const index = indexOf.call(this.items, document.activeElement)
+    this.items[index === -1 ? 0 : index + 1]?.focus()
+  }
+
+  #refreshIconPerLine() {
+    if (this.items.length === 0) {
+      this.iconsPerLine = 0
+      return
+    }
+
+    const previousY = this.items[0].getBoundingClientRect().y
+
+    for (let i = 1, l = this.items.length; i < l; i++) {
+      const { y } = this.items[i].getBoundingClientRect()
+      if (y !== previousY) {
+        this.iconsPerLine = i
+        break
+      }
+    }
+  }
+
+  items
+
+  setup() {
+    this.items = this.children[0].children
+    this.iconsPerLine = 0
+    const ro = new ResizeObserver(debounce(() => this.#refreshIconPerLine()))
+    ro.observe(this)
+    this.ctx.signal.addEventListener("abort", () => ro.disconnect())
   }
 
   render({ template }) {
