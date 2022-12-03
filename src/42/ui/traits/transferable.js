913ca66a655256596d006c4aaa7e15357c68661d
zo <zombectro@gmail.com>
2022-12-03 02:04:18 +0100

    Add dragEventDrive.js

--- src/42/ui/traits/transferable.js
+++ src/42/ui/traits/transferable.js
@@ -1,12 +1,10 @@
 import Trait from "../classes/Trait.js"
 import settings from "../../core/settings.js"
-import listen from "../../fabric/event/listen.js"
 import ensureElement from "../../fabric/dom/ensureElement.js"
-import dt from "../../core/dt.js"
 import uid from "../../core/uid.js"
 import noop from "../../fabric/type/function/noop.js"
 import ensureScopeSelector from "../../fabric/event/ensureScopeSelector.js"
-import SlideHint, { getIndex, getNewIndex } from "./transferable/SlideHint.js"
+import dragEventDriver from "./transferable/dragEventDriver.js"
 
 const DEFAULTS = {
   selector: ":scope > *",
@@ -16,13 +14,11 @@ const DEFAULTS = {
   effects: ["copy", "move", "link"],
   silentEffectCheck: false,
   fileSystemHandle: false,
+  driver: "dragEvent",
   hint: "slide",
 }
 
 const configure = settings("ui.trait.transferable", DEFAULTS)
-
-let hint
-let originHint
 
 class Transferable extends Trait {
   constructor(el, options) {
@@ -35,7 +31,7 @@ class Transferable extends Trait {
 
     this.config = configure(options)
 
-    const effects = this.list
+    this.effects = this.list
       ? options.effects ?? ["copy", "move"]
       : this.config.effects
 
@@ -115,147 +111,7 @@ class Transferable extends Trait {
       }
     }
 
-    let counter = 0
-    let justStarted = false
-
-    /* dropzone
-      =========== */
-
-    const { signal } = this.cancel
-
-    listen(this.dropzone, {
-      signal,
-      prevent: true,
-
-      dragover: (e) => {
-        dt.effects.handleEffect(e, this.config)
-        hint?.layout?.(e)
-      },
-
-      dragenter: (e) => {
-        dt.effects.handleEffect(e, this.config)
-        if (counter++ === 0) {
-          this.dropzone.classList.add("dragover")
-          if (hint) {
-            // force index to end of the list if no item is hovered before drop
-            hint.index =
-              this.list?.length ??
-              this.dropzone.querySelectorAll(this.selector).length
-
-            if (hint.id === id) {
-              hint.enterDropzone?.()
-            } else {
-              originHint = hint
-              hint = new SlideHint(this, {
-                x: e.x,
-                y: e.y,
-                target: originHint.ghost,
-                index: originHint.index,
-              })
-              originHint.ghost.style.opacity = 0
-            }
-          }
-        }
-      },
-
-      dragleave: () => {
-        if (--counter <= 0) {
-          counter = 0
-          this.dropzone.classList.remove("dragover")
-
-          if (originHint) {
-            hint.destroy()
-            hint = originHint
-            originHint = undefined
-          }
-
-          hint?.leaveDropzone?.()
-        }
-      },
-
-      drop: (e) => {
-        counter = 0
-        this.dropzone.classList.remove("dragover")
-
-        const res = dt.import(e, this.config)
-
-        if (hint) {
-          const { index } = hint
-          this.import(res, { index, dropzone: this.dropzone, x: e.x, y: e.y })
-          hint?.stop?.(e)
-        } else {
-          const item = e.target.closest(this.selector)
-          const index = getNewIndex(e.x, e.y, item, this.orientation)
-          this.import(res, { index, dropzone: this.dropzone, x: e.x, y: e.y })
-        }
-      },
-    })
-
-    if (!this.selector) return
-
-    /* draggable items
-      ================== */
-
-    listen(this.el, {
-      signal,
-      selector: this.selector,
-
-      pointerdown(e, target) {
-        target.draggable = true // force draggable on element
-      },
-
-      dragstart: (e, target) => {
-        counter = 0
-        this.isSorting = false
-        justStarted = true
-        const index = getIndex(target)
-        dt.export(e, { effects, data: this.export({ index, target }) })
-
-        if (this.config.hint === "slide") {
-          hint = new SlideHint(this, { x: e.x, y: e.y, target, index })
-        }
-      },
-
-      drag(e) {
-        if (justStarted) {
-          // fast drag sometimes don't trigger dragleave event
-          const isInDropzone = document
-            .elementFromPoint(e.x, e.y)
-            ?.closest(`#${id}`)
-          if (!isInDropzone) hint?.leave?.(e)
-          justStarted = false
-        }
-
-        hint?.update?.(e)
-      },
-
-      dragend: (e, target) => {
-        justStarted = false
-        counter = 0
-        this.dropzone.classList.remove("dragover")
-
-        if (originHint) {
-          originHint.destroy()
-          originHint = undefined
-        }
-
-        const { dropEffect } = e.dataTransfer
-
-        if (dropEffect === "move") hint?.destroy?.(e)
-        else hint?.revert?.(e)
-        hint = undefined
-
-        if (this.isSorting) {
-          this.isSorting = false
-          return
-        }
-
-        if (dropEffect === "move") {
-          const index = getIndex(target)
-          this.removeItem(index)
-        }
-      },
-    })
+    if (this.config.driver === "dragEvent") dragEventDriver(this)
   }
 }
 
