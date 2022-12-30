37989fe9b0d03a077d1fd2e0a390d134fb2ba11a
zo <zombectro@gmail.com>
2022-12-30 14:01:37 +0100

    Fix getRects.js

--- src/42/fabric/dom/getRects.js
+++ src/42/fabric/dom/getRects.js
@@ -11,22 +11,19 @@ export async function getRects(elements, options) {
     const observer = new IntersectionObserver(
       (entries) => {
         if (options?.relative) {
-          let { paddingLeft, paddingTop } = getComputedStyle(root)
-          paddingLeft = Number.parseInt(paddingLeft, 10)
-          paddingTop = Number.parseInt(paddingTop, 10)
-
-          const { rootBounds } = entries[0]
-          const rootX = rootBounds.x - paddingLeft - root.scrollLeft
-          const rootY = rootBounds.y - paddingTop - root.scrollTop
+          const rootRect = root.getBoundingClientRect()
+          const { borderLeftWidth, borderTopWidth } = getComputedStyle(root)
+          rootRect.x += Number.parseInt(borderLeftWidth, 10)
+          rootRect.y += Number.parseInt(borderTopWidth, 10)
 
           for (const { target, boundingClientRect } of entries) {
             const rect = boundingClientRect.toJSON()
             rect.target = target
 
-            rect.left -= rootX
-            rect.top -= rootY
-            rect.right -= rootX
-            rect.bottom -= rootY
+            rect.left -= rootRect.x
+            rect.top -= rootRect.y
+            rect.right -= rootRect.x
+            rect.bottom -= rootRect.y
 
             rect.x = rect.left
             rect.y = rect.top
