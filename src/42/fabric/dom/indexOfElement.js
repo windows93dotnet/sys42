7525d10284af1ef7df48e4c79327646ed7bd3203
zo <zombectro@gmail.com>
2023-01-07 03:35:03 +0100

    Improve getRects.js

--- src/42/fabric/dom/indexOfElement.js
+++ src/42/fabric/dom/indexOfElement.js
@@ -1,13 +1,19 @@
-export function indexOfElement(node, parent = node?.parentElement) {
+export function indexOfElement(el, parent = el?.parentElement) {
   if (!parent) return -1
 
   const { children } = parent
 
   for (let i = 0, l = children.length; i < l; i++) {
-    if (children[i] === node) return i
+    if (children[i] === el) return i
   }
 
   return -1
+}
+
+export function getElementIndex(el) {
+  let i = 0
+  while ((el = el.previousElementSibling)) i++
+  return i
 }
 
 export default indexOfElement
