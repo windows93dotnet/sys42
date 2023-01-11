39cae2771118dca585d25e02eb190d11e3e129ad
zo <zombectro@gmail.com>
2023-01-11 15:03:56 +0100

    Add accordion.css

--- src/42/ui/components/accordion.js
+++ src/42/ui/components/accordion.js
@@ -1,4 +1,5 @@
 import Component from "../classes/Component.js"
+import removeItem from "../../fabric/type/array/removeItem.js"
 
 export class Accordion extends Component {
   static definition = {
@@ -7,9 +8,9 @@ export class Accordion extends Component {
     id: true,
 
     props: {
-      current: {
-        type: "number",
-        default: 0,
+      expanded: {
+        type: "array",
+        default: [0, 1],
       },
       content: {
         type: "array",
@@ -27,7 +28,12 @@ export class Accordion extends Component {
   }
 
   selectPanel(index) {
-    console.log(index)
+    if (this.expanded.includes(index)) {
+      removeItem(this.expanded, index)
+    } else {
+      // this.expanded.length = 0
+      this.expanded.push(index)
+    }
   }
 
   render() {
@@ -37,20 +43,31 @@ export class Accordion extends Component {
         scope: "content",
         each: [
           {
-            tag: "h2.ui-accordion__label.button.pa-0",
+            tag: "h2.ui-accordion__label",
             content: {
-              tag: "button.ui-accordion__button.btn-clear",
+              tag: "button.ui-accordion__button",
+              picto: "puzzle",
               aria: {
                 controls: `${id}-panel-{{@index}}`,
-                expanded: "{{../../current === @index}}",
+                expanded: "{{includes(../../../expanded, @index)}}",
               },
               content: "{{render(label)}}",
+              click: "{{selectPanel(@index)}}",
             },
           },
           {
             tag: "section.ui-accordion__panel",
+            if: "{{includes(../../../expanded, @index)}}",
+            animate: {
+              flexBasis: "0%",
+              ms: 300,
+              start: { overflow: "hidden" },
+            },
             id: `${id}-panel-{{@index}}`,
-            content: "{{render(content)}}",
+            content: {
+              tag: ".ui-accordion__content",
+              content: "{{render(content)}}",
+            },
           },
         ],
       },
