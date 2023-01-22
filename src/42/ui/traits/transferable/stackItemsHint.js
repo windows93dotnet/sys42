ea90b8b1c80991995ad870a7708b6ea529687293
zo <zombectro@gmail.com>
2023-01-22 02:59:02 +0100

    Add ItemsHint getCoord

--- src/42/ui/traits/transferable/stackItemsHint.js
+++ src/42/ui/traits/transferable/stackItemsHint.js
@@ -1,34 +1,21 @@
 import ItemsHint from "./ItemsHint.js"
 
 export class StackItemsHint extends ItemsHint {
-  drag(x, y) {
-    super.drag(x, y)
+  drag({ x, y }) {
+    super.drag()
     if (!this.length) return
     const [first] = this
     first.ghost.style.zIndex = 1e5 + this.length
 
-    let translate
-
-    if (
-      this.currentZone?.isOriginDropzone &&
-      this.currentZone?.freeAxis !== true
-    ) {
-      translate = this.currentZone.isVertical
-        ? { x: first.x, y: y - first.offsetY }
-        : { x: x - first.offsetX, y: first.y }
-    } else {
-      translate = { x: x - first.offsetX, y: y - first.offsetY }
-    }
-
-    first.ghost.style.translate = `${translate.x}px ${translate.y}px`
+    first.ghost.style.translate = `${x}px ${y}px`
 
     for (let i = 1, l = this.length; i < l; i++) {
       const item = this[i]
       const offset = i * 3
       item.ghost.style.zIndex = 1e5 + this.length - i
       item.ghost.style.translate = `
-        ${translate.x + offset}px
-        ${translate.y + offset}px`
+        ${x + offset}px
+        ${y + offset}px`
     }
   }
 }
