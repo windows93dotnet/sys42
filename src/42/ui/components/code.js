17f0fc4728a2a81b114e9dae0e4febf5a180010c
zo <zombectro@gmail.com>
2023-01-23 15:37:27 +0100

    Improve template escaping

--- src/42/ui/components/code.js
+++ src/42/ui/components/code.js
@@ -3,6 +3,9 @@ import stringify from "../../fabric/type/any/stringify.js"
 import highlight from "../../core/console/formats/highlight.js"
 import logAsContent from "../../core/console/logAsContent.js"
 
+const OPEN = `#OPEN_TEMPLATE_${42}`
+const CLOSE = `#CLOSE_TEMPLATE_${42}`
+
 export class Code extends Component {
   static plan = {
     tag: "ui-code",
@@ -12,20 +15,31 @@ export class Code extends Component {
       value: {
         type: "any",
       },
+      escapeTemplate: false,
     },
   }
 
-  render({ content }) {
+  render({ content, escapeTemplate }) {
+    content = typeof content === "string" ? content : stringify.line(content)
+
+    content = content
+      .replaceAll(`{${"{"}`, OPEN) //
+      .replaceAll("}}", CLOSE)
+
+    content = highlight(content)
+
+    content = content
+      .replaceAll(OPEN, escapeTemplate ? "\\\\{\\{" : `{${"{"}`)
+      .replaceAll(CLOSE, escapeTemplate ? "\\}\\}" : "}}")
+
+    content = logAsContent(content)
+
     return [
       {
         tag: "pre.code.ma-0",
         content: {
           tag: "code",
-          content: logAsContent(
-            highlight(
-              typeof content === "string" ? content : stringify.line(content)
-            )
-          ),
+          content,
         },
       },
     ]
