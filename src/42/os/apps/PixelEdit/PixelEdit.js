a352cd2e9298dbe12c61f89d6f1a991cfe8f9681
zo <zombectro@gmail.com>
2023-02-05 04:25:28 +0100

    Prepare pixel editor

---
+++ src/42/os/apps/PixelEdit/PixelEdit.js
@@ -0,0 +1,37 @@
+export default {
+  menubar: [
+    { $ref: "FileMenu" }, //
+    { $ref: "HelpMenu" },
+  ],
+
+  content: {
+    tag: "ui-tabs",
+    current: "{{$current}}",
+    items: "{{$files}}",
+    tabTemplate: {
+      content: "{{dirty ? '*' : ''}}{{name}}",
+    },
+    panelTemplate: {
+      content: [
+        {
+          tag: "div.size-full.inset.checkboard",
+          content: "{{path}}",
+        },
+      ],
+    },
+    closable: true,
+    transferable: {
+      kind: ["$file", "$app"],
+      accept: "$file",
+      import({ paths, index, isOriginDropzone }) {
+        if (isOriginDropzone) return
+
+        index ??= $files.length
+        $files.splice(index, 0, ...paths)
+        $app.state.$current = index
+
+        return "vanish"
+      },
+    },
+  },
+}
