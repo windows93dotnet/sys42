eb544811533ff7c083837a4bc167f9bf2dca0661
zo <zombectro@gmail.com>
2023-02-08 10:04:13 +0100

    Improve focus management

--- src/42/ui/traits/navigable.js
+++ src/42/ui/traits/navigable.js
@@ -1,12 +1,20 @@
+/* eslint-disable object-shorthand */
 import Trait from "../classes/Trait.js"
 import settings from "../../core/settings.js"
 import focus, { TabOrder } from "../../fabric/dom/focus.js"
 import on from "../../fabric/event/on.js"
+import noop from "../../fabric/type/function/noop.js"
 
 const DEFAULTS = {
   selector: undefined,
-  loop: true,
-  remember: true,
+  loop: !false,
+  remember: !false,
+  shortcuts: {
+    next: "ArrowRight",
+    prev: "ArrowLeft",
+    first: "Home || PageUp",
+    last: "End || PageDown",
+  },
 }
 
 const configure = settings("ui.trait.navigable", DEFAULTS)
@@ -17,50 +25,90 @@ class Navigable extends Trait {
 
     this.config = configure(options)
 
+    this.el.tabIndex = -1
+
+    const { shortcuts } = this.config
+
     const { signal } = this.cancel
     const tabOrderOptions = { loop: this.config.loop }
 
-    this.el.tabIndex = -1
+    let fromPointerdown
 
-    let list
-    let remember
-
-    on(this.el, {
-      signal,
-      "disrupt": true,
-
-      "focusin": (e) => {
-        if (!this.el.contains(e.relatedTarget)) {
-          let ok
-          if (remember) ok = focus.attemptFocus(remember)
-          if (ok !== true) focus.first(this.el)
+    const remember = this.config.remember
+      ? (target = document.activeElement) => {
+          if (this.el.contains(target)) this.lastFocused = target
         }
-      },
+      : noop
 
-      "Tab": () => {
-        if (this.config.remember && this.el.contains(document.activeElement)) {
-          remember = document.activeElement
-        }
+    const blur = (target) => {
+      remember(target)
+      this.list = undefined
+    }
 
-        focus.next(this.el, this.el.parentElement)
+    on(
+      this.el,
+      { signal },
+      {
+        "pointerdown": () => {
+          fromPointerdown = true
+        },
+        "pointerup || pointercancel": () => {
+          fromPointerdown = false
+        },
+        "focusout": (e) => {
+          if (!this.el.contains(e.relatedTarget)) blur(e.target)
+        },
+        "focusin": (e) => {
+          if (fromPointerdown) return void remember(e.target)
+          if (!this.el.contains(e.relatedTarget)) {
+            if (this.lastFocused && focus.attemptFocus(this.lastFocused)) return
+            focus.first(this.el)
+          }
+        },
+        "focus": (e) => {
+          if (
+            !this.el.contains(e.relatedTarget) ||
+            document.activeElement === this.el
+          ) {
+            if (this.lastFocused && focus.attemptFocus(this.lastFocused)) return
+            focus.first(this.el)
+          }
+        },
       },
-      "Shift+Tab": () => {
-        if (this.config.remember && this.el.contains(document.activeElement)) {
-          remember = document.activeElement
-        }
+      {
+        "prevent": true,
 
-        focus.prev(this.el, this.el.parentElement)
-      },
+        "Tab": () => {
+          blur()
+          focus.next(this.el, this.el.parentElement)
+        },
+        "Shift+Tab": () => {
+          blur()
+          focus.prev(this.el, this.el.parentElement)
+        },
 
-      "ArrowRight": () => {
-        list ??= new TabOrder(this.el, tabOrderOptions)
-        list.next()
-      },
-      "ArrowLeft": () => {
-        list ??= new TabOrder(this.el, tabOrderOptions)
-        list.prev()
-      },
-    })
+        [shortcuts.next]: () => {
+          this.list ??= new TabOrder(this.el, tabOrderOptions)
+          this.list.next()
+        },
+        [shortcuts.prev]: () => {
+          this.list ??= new TabOrder(this.el, tabOrderOptions)
+          this.list.prev()
+        },
+        [shortcuts.first]: () => {
+          this.list ??= new TabOrder(this.el, tabOrderOptions)
+          this.list.first()
+        },
+        [shortcuts.last]: () => {
+          this.list ??= new TabOrder(this.el, tabOrderOptions)
+          this.list.last()
+        },
+      }
+    )
+  }
+
+  update() {
+    this.list = undefined
   }
 }
 
