0751c9a6a5ff09312e0f1e47ca0dcacac319b7da
zo <zombectro@gmail.com>
2023-02-11 07:45:02 +0100

    Add transferable items details

--- src/42/ui/traits/transferable/DropzoneHint.js
+++ src/42/ui/traits/transferable/DropzoneHint.js
@@ -189,16 +189,17 @@ export class DropzoneHint {
         index: this.newIndex,
         isOriginDropzone: this.isOriginDropzone,
         dropzone: this,
-        get paths() {
-          const paths = []
-          for (const item of system.transfer.items) {
-            const path =
-              item.data?.path ?? item.data ?? item.target.getAttribute("path")
-            if (path) paths.push(path)
-          }
+        ...system.transfer.items.details,
+        // get paths() {
+        //   const paths = []
+        //   for (const item of system.transfer.items) {
+        //     const path =
+        //       item.data?.path ?? item.data ?? item.target.getAttribute("path")
+        //     if (path) paths.push(path)
+        //   }
 
-          return paths
-        },
+        //   return paths
+        // },
       },
       system.transfer
     )
