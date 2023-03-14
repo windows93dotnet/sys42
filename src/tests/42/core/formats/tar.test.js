04ffa8ae08663e6122f8a2dafb64245bdd31b1c5
zo <zombectro@gmail.com>
2023-03-14 11:13:35 +0100

    Add all tar extract tests

--- src/tests/42/core/formats/tar.test.js
+++ src/tests/42/core/formats/tar.test.js
@@ -55,7 +55,6 @@ test.tasks(
     }),
 
     task({
-      // only: true,
       url: "/tests/fixtures/tar/multi-file.tar",
       files: ["i am file-1\n", "i am file-2\n"],
       headers: [
@@ -253,9 +252,24 @@ test.tasks(
     }),
 
     task({
-      skip: true,
       title: "gnu long path",
       url: "/tests/fixtures/tar/gnu-long-path.tar",
+      headers: [
+        {
+          name: "node-v0.11.14/deps/npm/node_modules/init-package-json/node_modules/promzard/example/npm-init/init-in",
+          mode: 436,
+          uid: 0,
+          gid: 0,
+          size: 6058,
+          mtime: 1_411_595_201_000,
+          type: "file",
+          linkname: null,
+          uname: "root",
+          gname: "root",
+          devmajor: 0,
+          devminor: 0,
+        },
+      ],
     }),
 
     task({
@@ -267,6 +281,163 @@ test.tasks(
           gid: 1_876_110_778,
         },
       ],
+    }),
+
+    task({
+      title: "base 256 size",
+      url: "/tests/fixtures/tar/base-256-size.tar",
+      headers: [
+        {
+          name: "test.txt",
+          mode: 0o644,
+          uid: 501,
+          gid: 20,
+          size: 12,
+          mtime: 1_387_580_181_000,
+          type: "file",
+          linkname: null,
+          uname: "maf",
+          gname: "staff",
+          devmajor: 0,
+          devminor: 0,
+        },
+      ],
+    }),
+
+    task({
+      title: "latin-1",
+      url: "/tests/fixtures/tar/latin1.tar",
+      options: { filenameEncoding: "ascii" },
+      headers: [
+        {
+          name: "En français, s'il vous plaît?.txt",
+          mode: 0o644,
+          uid: 0,
+          gid: 0,
+          size: 14,
+          mtime: 1_495_941_034_000,
+          type: "file",
+          linkname: null,
+          uname: "root",
+          gname: "root",
+          devmajor: 0,
+          devminor: 0,
+        },
+      ],
+    }),
+
+    task({
+      url: "/tests/fixtures/tar/incomplete.tar",
+      throws: "Unexpected end of data",
+    }),
+
+    task({
+      url: "/tests/fixtures/tar/incomplete.tar",
+      options: { allowIncomplete: true },
+      files: ["i am file-1\n"],
+      headers: [
+        {
+          name: "file-1.txt",
+          mode: 420,
+          uid: 501,
+          gid: 20,
+          size: 12,
+          mtime: 1_387_580_181_000,
+          type: "file",
+          linkname: null,
+          uname: "maf",
+          gname: "staff",
+          devmajor: 0,
+          devminor: 0,
+        },
+      ],
+    }),
+
+    task({
+      // can correctly unpack gnu-tar format
+
+      // Created using gnu tar: tar cf gnu.tar --format gnu --owner=myuser:12345 --group=mygroup:67890 test.txt
+      url: "/tests/fixtures/tar/gnu.tar",
+      files: ["Hello, world!\n"],
+      headers: [
+        {
+          name: "test.txt",
+          mode: 0o644,
+          uid: 12_345,
+          gid: 67_890,
+          size: 14,
+          mtime: 1_559_239_869_000,
+          type: "file",
+          linkname: null,
+          uname: "myuser",
+          gname: "mygroup",
+          devmajor: 0,
+          devminor: 0,
+        },
+      ],
+    }),
+
+    task({
+      // can correctly unpack gnu-tar incremental format. In this situation,
+      // the tarball will have additional ctime and atime values in the header,
+      // and without awareness of the 'gnu' tar format, the atime (offset 345) is mistaken
+      // for a directory prefix (also offset 345).
+
+      // Created using gnu tar: tar cf gnu-incremental.tar -G --format gnu --owner=myuser:12345 --group=mygroup:67890 test.txt
+      url: "/tests/fixtures/tar/gnu-incremental.tar",
+      files: ["Hello, world!\n"],
+      headers: [
+        {
+          name: "test.txt",
+          mode: 0o644,
+          uid: 12_345,
+          gid: 67_890,
+          size: 14,
+          mtime: 1_559_239_869_000,
+          type: "file",
+          linkname: null,
+          uname: "myuser",
+          gname: "mygroup",
+          devmajor: 0,
+          devminor: 0,
+        },
+      ],
+    }),
+
+    task({
+      title: "v7",
+      // Created using gnu tar: tar cf v7.tar --format v7 test.txt
+      url: "/tests/fixtures/tar/v7.tar",
+      throws: "Invalid tar header: unknown format.",
+    }),
+
+    task({
+      title: "v7",
+      url: "/tests/fixtures/tar/v7.tar",
+      options: { allowUnknownFormat: true },
+      files: ["Hello, world!\n"],
+      headers: [
+        {
+          name: "test.txt",
+          mode: 420,
+          uid: 502,
+          gid: 20,
+          size: 14,
+          mtime: 1_559_246_975_000,
+          type: "file",
+          linkname: null,
+          uname: "",
+          gname: "",
+          devmajor: 0,
+          devminor: 0,
+        },
+      ],
+    }),
+
+    task({
+      // Created from multi-file.tar, removing the magic and recomputing the checksum
+      url: "/tests/fixtures/tar/unknown-format.tar",
+      throws: "Invalid tar header: unknown format.",
     }),
 
     task({
@@ -305,11 +476,6 @@ test.tasks(
       ],
     }),
 
-    task({
-      url: "/tests/fixtures/tar/unknown-format.tar",
-      throws: "Invalid tar header: unknown format.",
-    }),
-
     // task({
     //   only: true,
     //   url: "/tests/fixtures/tar/huge.tar.gz",
@@ -320,7 +486,7 @@ test.tasks(
 
   (test, { title, url, options, files, headers, length, throws }) => {
     if (throws) {
-      test("extract", "throws", title ?? getBasename(url), async (t) => {
+      test("extract", title ?? getBasename(url), "throws", async (t) => {
         t.timeout(1000)
         await t.throws(
           () =>
