a30c2fd02873e564ca7f26f673a3d3ac8db5fc6a
zo <zombectro@gmail.com>
2023-03-16 18:35:14 +0100

    Implement File as entry in tsTarPack

--- src/42/core/formats/tar/tsTarPack.js
+++ src/42/core/formats/tar/tsTarPack.js
@@ -1,32 +1,89 @@
 import headers from "./headers.js"
 import Buffer from "../../../fabric/binary/Buffer.js"
+import defer from "../../../fabric/type/promise/defer.js"
 
 const END_OF_TAR = new Uint8Array(1024)
 
+const S_IFMT = 0xf0_00
+const S_IFBLK = 0x60_00
+const S_IFCHR = 0x20_00
+const S_IFDIR = 0x40_00
+const S_IFIFO = 0x10_00
+const S_IFLNK = 0xa0_00
+
 const overflow = function (writer, size) {
   size &= 511
-  if (size) writer.write(END_OF_TAR.slice(0, 512 - size))
+  if (size) return writer.write(END_OF_TAR.slice(0, 512 - size))
 }
 
-export function rsTarPack() {
+function modeToType(mode) {
+  // prettier-ignore
+  switch (mode & S_IFMT) {
+    case S_IFBLK: return "block-device"
+    case S_IFCHR: return "character-device"
+    case S_IFDIR: return "directory"
+    case S_IFIFO: return "fifo"
+    case S_IFLNK: return "symlink"
+    default: return "file"
+  }
+}
+
+function normalizeHeader(header) {
+  if (header.size === undefined || header.type === "symlink") header.size = 0
+  header.type ??= modeToType(header.mode)
+  header.mode ??= header.type === "directory" ? 0o755 : 0o644
+  header.uid ??= 0
+  header.gid ??= 0
+  header.mtime ??= Date.now()
+  return header
+}
+
+export function tsTarPack() {
   const ts = new TransformStream()
 
-  const writer = ts.writable.getWriter()
+  let writer = ts.writable.getWriter()
+  let busy
 
-  ts.entry = function (header, file) {
-    const buf = Buffer.from(file)
-    header.size ??= buf.length
-    writer.write(headers.encode(header))
-    writer.write(buf)
-    overflow(writer, header.size)
+  ts.add = async function (header, file) {
+    await busy
+    busy = defer()
+
+    if (header instanceof File) {
+      file = header
+      header = {}
+    }
+
+    if (file instanceof File) {
+      header.name ??= file.name
+      header.size ??= file.size
+      header.mtime ??= file.lastModified
+      writer.write(headers.encode(normalizeHeader(header)))
+      writer.releaseLock()
+      await file.stream().pipeTo(ts.writable, { preventClose: true })
+      writer = ts.writable.getWriter()
+      overflow(writer, header.size)
+    } else {
+      const buf = Buffer.from(file)
+      header.size ??= buf.length
+      writer.write(headers.encode(normalizeHeader(header)))
+      writer.write(buf)
+      overflow(writer, header.size)
+    }
+
+    busy.resolve()
   }
 
-  ts.finalize = function () {
-    writer.write(END_OF_TAR)
-    writer.close()
+  ts.stream = function () {
+    queueMicrotask(async () => {
+      await 0
+      await busy
+      writer.write(END_OF_TAR)
+      writer.close()
+    })
+    return ts.readable
   }
 
   return ts
 }
 
-export default rsTarPack
+export default tsTarPack
--- src/tests/42/core/formats/tar/tsTarPack.test.js
+++ src/tests/42/core/formats/tar/tsTarPack.test.js
@@ -1,16 +1,16 @@
 /* spell-checker: disable */
 
 import test from "../../../../../42/test.js"
-import rsTarPack from "../../../../../42/core/formats/tar/tsTarPack.js"
+import tsTarPack from "../../../../../42/core/formats/tar/tsTarPack.js"
 
 const { stream, load } = test.utils
 
 test("pack", async (t) => {
-  t.timeout(800)
+  t.timeout(1000)
 
-  const pack = rsTarPack()
+  const pack = tsTarPack()
 
-  pack.entry(
+  pack.add(
     {
       name: "test.txt",
       mtime: 1_387_580_181_000,
@@ -23,11 +23,54 @@ test("pack", async (t) => {
     "hello world\n"
   )
 
-  pack.finalize()
+  const [actual, expected] = await Promise.all([
+    stream.ws.collect(pack.stream()),
+    load.arrayBuffer("/tests/fixtures/tar/one-file.tar"),
+  ])
+
+  t.eq(actual, expected)
+})
+
+test("pack", "File", async (t) => {
+  t.timeout(1000)
+
+  const pack = tsTarPack()
+
+  pack.add(
+    {
+      mode: 0o644,
+      uname: "maf",
+      gname: "staff",
+      uid: 501,
+      gid: 20,
+    },
+    new File(["hello world\n"], "test.txt", {
+      lastModified: 1_387_580_181_000,
+    })
+  )
 
   const [actual, expected] = await Promise.all([
-    stream.ws.collect(pack.readable),
+    stream.ws.collect(pack.stream()),
     load.arrayBuffer("/tests/fixtures/tar/one-file.tar"),
+  ])
+
+  t.eq(actual, expected)
+})
+
+test("pack", "File without header", async (t) => {
+  t.timeout(1000)
+
+  const pack = tsTarPack()
+
+  pack.add(
+    new File(["hello world\n"], "test.txt", {
+      lastModified: 1_387_580_181_000,
+    })
+  )
+
+  const [actual, expected] = await Promise.all([
+    stream.ws.collect(pack.stream()),
+    load.arrayBuffer("/tests/fixtures/tar/hello-world.tar"),
   ])
 
   t.eq(actual, expected)
