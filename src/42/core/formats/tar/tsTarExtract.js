edf7804d768d6cfee3e9041625b3559d64357a14
zo <zombectro@gmail.com>
2023-03-16 04:00:10 +0100

    Add tsTarExtract.js

---
+++ src/42/core/formats/tar/tsTarExtract.js
@@ -0,0 +1,154 @@
+import Buffer from "../../../fabric/binary/Buffer.js"
+import headers from "./headers.js"
+import getBasename from "../../path/core/getBasename.js"
+
+// @src https://deno.land/std@0.162.0/streams/buffer.ts?source#L247
+function tsRange(start = 0, end = Infinity) {
+  let offsetStart = 0
+  let offsetEnd = 0
+  return new TransformStream({
+    transform(chunk, controller) {
+      offsetStart = offsetEnd
+      offsetEnd += chunk.byteLength
+      if (offsetEnd > start) {
+        if (offsetStart < start) {
+          chunk = chunk.slice(start - offsetStart)
+        }
+
+        if (offsetEnd >= end) {
+          chunk = chunk.slice(0, chunk.byteLength - offsetEnd + end)
+          controller.enqueue(chunk)
+          controller.terminate()
+        } else {
+          controller.enqueue(chunk)
+        }
+      }
+    },
+  })
+}
+
+function overflow(size) {
+  size &= 511
+  return size && 512 - size
+}
+
+function makeFile(carrier, offset, header, enqueue) {
+  Object.defineProperties(header, {
+    file: {
+      async value() {
+        const res = new Response(header.stream)
+        return new File([await res.arrayBuffer()], getBasename(header.name), {
+          lastModified: header.mtime,
+        })
+      },
+    },
+
+    stream: {
+      get() {
+        const [a, b] = carrier.readable.tee()
+        carrier.readable = a
+        return b.pipeThrough(tsRange(offset, offset + header.size))
+      },
+    },
+  })
+  enqueue(header)
+  return header.size + overflow(header.size)
+}
+
+function mixinPax(header, pax) {
+  if (pax.path) header.name = pax.path
+  if (pax.linkpath) header.linkname = pax.linkpath
+  if (pax.size > 0) header.size = Number.parseInt(pax.size, 10)
+  header.pax = pax
+  return header
+}
+
+function createConsumer(carrier, enqueue) {
+  const buffer = new Buffer()
+
+  let header
+  let pax
+
+  function consume() {
+    if (header) {
+      if (buffer.length < buffer.offset + header.size) return
+
+      if (header.type === "file") {
+        if (pax) mixinPax(header, pax)
+        buffer.offset += makeFile(carrier, buffer.offset, header, enqueue)
+        header = undefined
+        consume()
+      } else if (header.type === "pax-header") {
+        pax = headers.decodePax(buffer.read(header.size))
+        buffer.offset += overflow(header.size)
+        header = undefined
+        consume()
+      } else if (header.type === "gnu-long-path") {
+        header.name = headers.decodeLongPath(
+          buffer.read(header.size),
+          carrier.options?.filenameEncoding
+        )
+        buffer.offset += overflow(header.size)
+        header = undefined
+        consume()
+      }
+    } else if (buffer.length > buffer.offset + 512) {
+      header = headers.decode(buffer.read(512), carrier.options)
+
+      if (header?.size === 0 || header?.type === "directory") {
+        enqueue(header)
+        header = undefined
+      }
+
+      consume()
+    }
+  }
+
+  return { buffer, consume }
+}
+
+export function tsTarExtract(options) {
+  let buffer
+  let consume
+
+  const carrier = { options, missing: 0 }
+
+  const tsExtract = new TransformStream({
+    start(controller) {
+      const consumer = createConsumer(carrier, (chunk) =>
+        controller.enqueue(chunk)
+      )
+      buffer = consumer.buffer
+      consume = consumer.consume
+    },
+    async transform(chunk) {
+      buffer.write(chunk)
+      consume()
+    },
+    flush() {
+      if (buffer.length < buffer.offset && options?.allowIncomplete !== true) {
+        throw new Error("Unexpected end of data")
+      }
+
+      delete buffer.memory
+      buffer = undefined
+    },
+  })
+
+  const ts = new TransformStream()
+
+  let { readable } = ts
+
+  if (options?.gzip) {
+    readable = readable.pipeThrough(new DecompressionStream("gzip"))
+  }
+
+  const [a, b] = readable.tee()
+
+  carrier.readable = a
+  Object.defineProperty(ts, "readable", { value: b.pipeThrough(tsExtract) })
+
+  return ts
+}
+
+export default tsTarExtract
---
+++ src/tests/42/core/formats/tar/tsTarExtract.test.js
@@ -0,0 +1,550 @@
+/* spell-checker: disable */
+
+import test from "../../../../../42/test.js"
+import tsTarExtract from "../../../../../42/core/formats/tar/tsTarExtract.js"
+import getBasename from "../../../../../42/core/path/core/getBasename.js"
+
+const { stream, http } = test.utils
+const { task } = test
+
+// @src https://github.com/mafintosh/tar-stream/blob/master/test/extract.js
+
+test.tasks(
+  [
+    task({
+      url: "/tests/fixtures/tar/one-file.tar",
+      files: ["hello world\n"],
+      headers: [
+        {
+          name: "test.txt",
+          mode: 0o644,
+          uid: 501,
+          gid: 20,
+          size: 12,
+          mtime: 1_387_580_181_000,
+          type: "file",
+          linkname: null,
+          uname: "maf",
+          gname: "staff",
+          devmajor: 0,
+          devminor: 0,
+        },
+      ],
+    }),
+
+    task({
+      url: "/tests/fixtures/tar/one-file.tar.gz",
+      options: { gzip: true },
+      files: ["hello world\n"],
+      headers: [
+        {
+          name: "test.txt",
+          mode: 0o644,
+          uid: 501,
+          gid: 20,
+          size: 12,
+          mtime: 1_387_580_181_000,
+          type: "file",
+          linkname: null,
+          uname: "maf",
+          gname: "staff",
+          devmajor: 0,
+          devminor: 0,
+        },
+      ],
+    }),
+
+    task({
+      url: "/tests/fixtures/tar/multi-file.tar",
+      files: ["i am file-1\n", "i am file-2\n"],
+      headers: [
+        {
+          name: "file-1.txt",
+          mode: 0o644,
+          uid: 501,
+          gid: 20,
+          size: 12,
+          mtime: 1_387_580_181_000,
+          type: "file",
+          linkname: null,
+          uname: "maf",
+          gname: "staff",
+          devmajor: 0,
+          devminor: 0,
+        },
+        {
+          name: "file-2.txt",
+          mode: 0o644,
+          uid: 501,
+          gid: 20,
+          size: 12,
+          mtime: 1_387_580_181_000,
+          type: "file",
+          linkname: null,
+          uname: "maf",
+          gname: "staff",
+          devmajor: 0,
+          devminor: 0,
+        },
+      ],
+    }),
+
+    task({
+      url: "/tests/fixtures/tar/pax.tar",
+      files: ["hello world\n"],
+      headers: [
+        {
+          name: "pax.txt",
+          mode: 0o644,
+          uid: 501,
+          gid: 20,
+          size: 12,
+          mtime: 1_387_580_181_000,
+          type: "file",
+          linkname: null,
+          uname: "maf",
+          gname: "staff",
+          devmajor: 0,
+          devminor: 0,
+          pax: { path: "pax.txt", special: "sauce" },
+        },
+      ],
+    }),
+
+    task({
+      url: "/tests/fixtures/tar/types.tar",
+      headers: [
+        {
+          name: "directory",
+          mode: 0o755,
+          uid: 501,
+          gid: 20,
+          size: 0,
+          mtime: 1_387_580_181_000,
+          type: "directory",
+          linkname: null,
+          uname: "maf",
+          gname: "staff",
+          devmajor: 0,
+          devminor: 0,
+        },
+        {
+          name: "directory-link",
+          mode: 0o755,
+          uid: 501,
+          gid: 20,
+          size: 0,
+          mtime: 1_387_580_181_000,
+          type: "symlink",
+          linkname: "directory",
+          uname: "maf",
+          gname: "staff",
+          devmajor: 0,
+          devminor: 0,
+        },
+      ],
+    }),
+
+    task({
+      url: "/tests/fixtures/tar/long-name.tar",
+      files: ["hello long name\n"],
+      headers: [
+        {
+          name: "my/file/is/longer/than/100/characters/and/should/use/the/prefix/header/foobarbaz/foobarbaz/foobarbaz/foobarbaz/foobarbaz/foobarbaz/filename.txt",
+          mode: 0o644,
+          uid: 501,
+          gid: 20,
+          size: 16,
+          mtime: 1_387_580_181_000,
+          type: "file",
+          linkname: null,
+          uname: "maf",
+          gname: "staff",
+          devmajor: 0,
+          devminor: 0,
+        },
+      ],
+    }),
+
+    task({
+      url: "/tests/fixtures/tar/unicode-bsd.tar",
+      files: ["hej\n"],
+      headers: [
+        {
+          name: "høllø.txt",
+          mode: 0o644,
+          uid: 501,
+          gid: 20,
+          size: 4,
+          mtime: 1_387_588_646_000,
+          pax: {
+            "SCHILY.dev": "16777217",
+            "SCHILY.ino": "3599143",
+            "SCHILY.nlink": "1",
+            "atime": "1387589077",
+            "ctime": "1387588646",
+            "path": "høllø.txt",
+          },
+          type: "file",
+          linkname: null,
+          uname: "maf",
+          gname: "staff",
+          devmajor: 0,
+          devminor: 0,
+        },
+      ],
+    }),
+
+    task({
+      url: "/tests/fixtures/tar/unicode.tar",
+      files: ["høllø\n"],
+      headers: [
+        {
+          name: "høstål.txt",
+          mode: 0o644,
+          uid: 501,
+          gid: 20,
+          size: 8,
+          mtime: 1_387_580_181_000,
+          pax: { path: "høstål.txt" },
+          type: "file",
+          linkname: null,
+          uname: "maf",
+          gname: "staff",
+          devmajor: 0,
+          devminor: 0,
+        },
+      ],
+    }),
+
+    task({
+      url: "/tests/fixtures/tar/name-is-100.tar",
+      files: ["hello\n"],
+      headers: [
+        {
+          name: "node_modules/mocha-jshint/node_modules/jshint/node_modules/console-browserify/test/static/index.html",
+          mode: 420,
+          uid: 501,
+          gid: 0,
+          size: 6,
+          mtime: 1_389_643_870_000,
+          type: "file",
+          linkname: null,
+          uname: "maf",
+          gname: "wheel",
+          devmajor: 0,
+          devminor: 0,
+        },
+      ],
+    }),
+
+    task({
+      url: "/tests/fixtures/tar/invalid.tgz",
+      // options: { gzip: true },
+      throws:
+        "Invalid tar header. Maybe the tar is corrupted or it needs to be gunzipped?",
+    }),
+
+    task({
+      title: "space prefixed",
+      url: "/tests/fixtures/tar/space.tar",
+      length: 4,
+    }),
+
+    task({
+      title: "gnu long path",
+      url: "/tests/fixtures/tar/gnu-long-path.tar",
+      headers: [
+        {
+          name: "node-v0.11.14/deps/npm/node_modules/init-package-json/node_modules/promzard/example/npm-init/init-in",
+          mode: 436,
+          uid: 0,
+          gid: 0,
+          size: 6058,
+          mtime: 1_411_595_201_000,
+          type: "file",
+          linkname: null,
+          uname: "root",
+          gname: "root",
+          devmajor: 0,
+          devminor: 0,
+        },
+      ],
+    }),
+
+    task({
+      url: "/tests/fixtures/tar/base-256-uid-gid.tar",
+      headers: [
+        {
+          uid: 116_435_139,
+          gid: 1_876_110_778,
+        },
+      ],
+    }),
+
+    task({
+      url: "/tests/fixtures/tar/base-256-size.tar",
+      headers: [
+        {
+          name: "test.txt",
+          mode: 0o644,
+          uid: 501,
+          gid: 20,
+          size: 12,
+          mtime: 1_387_580_181_000,
+          type: "file",
+          linkname: null,
+          uname: "maf",
+          gname: "staff",
+          devmajor: 0,
+          devminor: 0,
+        },
+      ],
+    }),
+
+    task({
+      title: "latin-1",
+      url: "/tests/fixtures/tar/latin1.tar",
+      options: { filenameEncoding: "ascii" },
+      headers: [
+        {
+          name: "En français, s'il vous plaît?.txt",
+          mode: 0o644,
+          uid: 0,
+          gid: 0,
+          size: 14,
+          mtime: 1_495_941_034_000,
+          type: "file",
+          linkname: null,
+          uname: "root",
+          gname: "root",
+          devmajor: 0,
+          devminor: 0,
+        },
+      ],
+    }),
+
+    task({
+      url: "/tests/fixtures/tar/incomplete.tar",
+      throws: "Unexpected end of data",
+    }),
+
+    task({
+      url: "/tests/fixtures/tar/incomplete.tar",
+      options: { allowIncomplete: true },
+      files: ["i am file-1\n"],
+      headers: [
+        {
+          name: "file-1.txt",
+          mode: 420,
+          uid: 501,
+          gid: 20,
+          size: 12,
+          mtime: 1_387_580_181_000,
+          type: "file",
+          linkname: null,
+          uname: "maf",
+          gname: "staff",
+          devmajor: 0,
+          devminor: 0,
+        },
+      ],
+    }),
+
+    task({
+      // can correctly unpack gnu-tar format
+
+      // Created using gnu tar: tar cf gnu.tar --format gnu --owner=myuser:12345 --group=mygroup:67890 test.txt
+      url: "/tests/fixtures/tar/gnu.tar",
+      files: ["Hello, world!\n"],
+      headers: [
+        {
+          name: "test.txt",
+          mode: 0o644,
+          uid: 12_345,
+          gid: 67_890,
+          size: 14,
+          mtime: 1_559_239_869_000,
+          type: "file",
+          linkname: null,
+          uname: "myuser",
+          gname: "mygroup",
+          devmajor: 0,
+          devminor: 0,
+        },
+      ],
+    }),
+
+    task({
+      // can correctly unpack gnu-tar incremental format. In this situation,
+      // the tarball will have additional ctime and atime values in the header,
+      // and without awareness of the 'gnu' tar format, the atime (offset 345) is mistaken
+      // for a directory prefix (also offset 345).
+
+      // Created using gnu tar: tar cf gnu-incremental.tar -G --format gnu --owner=myuser:12345 --group=mygroup:67890 test.txt
+      url: "/tests/fixtures/tar/gnu-incremental.tar",
+      files: ["Hello, world!\n"],
+      headers: [
+        {
+          name: "test.txt",
+          mode: 0o644,
+          uid: 12_345,
+          gid: 67_890,
+          size: 14,
+          mtime: 1_559_239_869_000,
+          type: "file",
+          linkname: null,
+          uname: "myuser",
+          gname: "mygroup",
+          devmajor: 0,
+          devminor: 0,
+        },
+      ],
+    }),
+
+    task({
+      title: "v7",
+      // Created using gnu tar: tar cf v7.tar --format v7 test.txt
+      url: "/tests/fixtures/tar/v7.tar",
+      throws: "Invalid tar header: unknown format.",
+    }),
+
+    task({
+      title: "v7",
+      url: "/tests/fixtures/tar/v7.tar",
+      options: { allowUnknownFormat: true },
+      files: ["Hello, world!\n"],
+      headers: [
+        {
+          name: "test.txt",
+          mode: 420,
+          uid: 502,
+          gid: 20,
+          size: 14,
+          mtime: 1_559_246_975_000,
+          type: "file",
+          linkname: null,
+          uname: "",
+          gname: "",
+          devmajor: 0,
+          devminor: 0,
+        },
+      ],
+    }),
+
+    task({
+      // Created from multi-file.tar, removing the magic and recomputing the checksum
+      url: "/tests/fixtures/tar/unknown-format.tar",
+      throws: "Invalid tar header: unknown format.",
+    }),
+
+    task({
+      url: "/tests/fixtures/tar/unknown-format.tar",
+      options: { allowUnknownFormat: true },
+      files: ["i am file-1\n", "i am file-2\n"],
+      headers: [
+        {
+          name: "file-1.txt",
+          mode: 0o644,
+          uid: 501,
+          gid: 20,
+          size: 12,
+          mtime: 1_387_580_181_000,
+          type: "file",
+          linkname: null,
+          uname: "maf",
+          gname: "staff",
+          devmajor: 0,
+          devminor: 0,
+        },
+        {
+          name: "file-2.txt",
+          mode: 0o644,
+          uid: 501,
+          gid: 20,
+          size: 12,
+          mtime: 1_387_580_181_000,
+          type: "file",
+          linkname: null,
+          uname: "maf",
+          gname: "staff",
+          devmajor: 0,
+          devminor: 0,
+        },
+      ],
+    }),
+
+    // task({
+    //   only: true,
+    //   url: "/tests/fixtures/tar/huge.tar.gz",
+    //   options: { gzip: true },
+    //   length: 1,
+    // }),
+  ],
+
+  (test, { title, url, options, files, headers, length, throws }) => {
+    if (throws) {
+      test("extract", title ?? getBasename(url), "throws", async (t) => {
+        t.timeout(1000)
+        await t.throws(
+          () =>
+            stream.ws.collect(
+              http
+                .source(url)
+                .pipeThrough(stream.ts.cut(321))
+                .pipeThrough(tsTarExtract(options))
+            ),
+          throws
+        )
+      })
+      return
+    }
+
+    test("extract", title ?? getBasename(url), async (t) => {
+      t.timeout(1000)
+
+      const items = await stream.ws.collect(
+        http
+          .source(url)
+          .pipeThrough(stream.ts.cut(321))
+          .pipeThrough(tsTarExtract(options))
+      )
+
+      if (files) {
+        t.eq(
+          await Promise.all(
+            items.map(async ({ file }) => (await file()).text())
+          ),
+          files
+        )
+
+        t.eq(
+          await Promise.all(
+            items.map(async (item) =>
+              stream.ws.collect(item.stream.pipeThrough(stream.ts.text()))
+            )
+          ),
+          files
+        )
+      }
+
+      if (headers) {
+        if (Object.keys(headers[0]).length === 12) {
+          t.eq(
+            items.map((header) => ({ ...header })),
+            headers
+          )
+        } else {
+          t.hasSubset(
+            items.map((header) => ({ ...header })),
+            headers
+          )
+        }
+      }
+
+      length ??= files?.length ?? headers?.length
+
+      if (length !== undefined) t.is(items.length, length)
+    })
+  }
+)
