2908761ff90afda503a55a484d93ec7398181d91
zo <zombectro@gmail.com>
2023-11-30 22:04:20 +0100

    Fix shortenFilename tests on simple server

--- src/tests/42/core/path/shortenFilename.test.js
+++ src/tests/42/core/path/shortenFilename.test.js
@@ -4,12 +4,16 @@ import resolvePath from "../../../../42/core/path/core/resolvePath.js"
 
 const { task } = test
 
+const origin = test.env.runtime.inFrontend
+  ? location.origin
+  : "http://localhost:4200"
+
 test.tasks(
   [
     task({
-      path: "http://localhost:4200/tests/xxx.test.js",
-      backend: "./src/tests/xxx.test.js",
-      frontend: "/tests/xxx.test.js",
+      path: `${origin}/tests/foo.test.js`,
+      backend: "./src/tests/foo.test.js",
+      frontend: "/tests/foo.test.js",
     }),
     task({
       path: resolvePath(`tests/a.test.js`),
