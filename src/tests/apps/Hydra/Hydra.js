1a6127b07c96c109c1ee1e365272489d74d9a142
zo <zombectro@gmail.com>
2024-03-09 19:43:10 +0100

    Fix Hydra app

--- src/tests/apps/Hydra/Hydra.js
+++ src/tests/apps/Hydra/Hydra.js
@@ -1,16 +1,11 @@
 import alert from "../../../42/ui/invocables/alert.js"
 
-const icon = new URL("./icon-32.png", import.meta.url).href
-
 async function hydra() {
   await alert({
-    dialog: {
-      style: { position: "absolute", top: 0, left: 0 },
-      x: Math.round(Math.random() * (window.innerWidth - 300)),
-      y: Math.round(Math.random() * (window.innerHeight - 118)),
-    },
     label: "HYDRA",
-    img: icon,
+    img: $manifest.icons[0].src,
+    x: Math.round(Math.random() * (window.innerWidth - 300)),
+    y: Math.round(Math.random() * (window.innerHeight - 118)),
     content: [
       "Cut off a head, two more will take its place.",
       "<br>",
