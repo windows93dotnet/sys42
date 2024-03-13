38d44611235ff9ee8d95275a92578ecbb5be82e8
zo <zombectro@gmail.com>
2024-03-13 18:47:24 +0100

    Improve isSerializable and isTransferable

--- src/42/fabric/type/any/is/isSerializable.js
+++ src/42/fabric/type/any/is/isSerializable.js
@@ -1,53 +1,59 @@
-// [1] wait for firefox implementation
-// https://bugzilla.mozilla.org/show_bug.cgi?id=1556604
-// https://bugzilla.mozilla.org/show_bug.cgi?id=1749547
+import isInstanceOf from "./isInstanceOf.js"
 
-// @src https://developer.mozilla.org/en-US/docs/Glossary/Transferable_objects#supported_objects
-const TRANSFERABLES = new Set([
-  // "ArrayBuffer",
-  "MessagePort",
-  // "ReadableStream", [1]
-  // "WritableStream", [1]
-  // "TransformStream", [1]
-  "AudioData",
-  "ImageBitmap",
-  "VideoFrame",
-  "OffscreenCanvas",
+export const PRIMITIVES = new Set([
+  "boolean", //
+  "string",
+  "number",
+  "bigint",
 ])
 
 // @src https://developer.mozilla.org/en-US/docs/Web/API/Web_Workers_API/Structured_clone_algorithm#supported_types
-const CLONEABLE = new Set([
+// @src https://developer.mozilla.org/en-US/docs/Web/API/Web_Workers_API/Structured_clone_algorithm#webapi_types
+export const SERIALIZABLES = new Set([
+  "Array",
+  "ArrayBuffer",
   "Boolean",
-  "String",
+  "DataView",
   "Date",
+  "Map",
+  "Number",
+  "Object",
   "RegExp",
+  "Set",
+  "String",
+  "AudioData",
   "Blob",
+  "CropTarget",
+  "CryptoKey",
+  "DOMException",
+  "DOMMatrix",
+  "DOMMatrixReadOnly",
+  "DOMPoint",
+  "DOMPointReadOnly",
+  "DOMQuad",
+  "DOMRect",
+  "DOMRectReadOnly",
   "File",
   "FileList",
-  "ArrayBuffer",
-  "ArrayBufferView",
+  "FileSystemDirectoryHandle",
+  "FileSystemFileHandle",
+  "FileSystemHandle",
+  "GPUCompilationInfo",
+  "GPUCompilationMessage",
   "ImageBitmap",
   "ImageData",
-  "Array",
-  "Object",
-  "Map",
-  "Set",
-  // "Error", [1]
-  "DOMRect",
+  "RTCCertificate",
+  "VideoFrame",
 ])
 
-const PRIMITIVES = new Set(["boolean", "string", "number", "bigint"])
-
-const { toString } = Object.prototype
-
-export default function isSerializable(value) {
-  if (!value) return 1
-  const type = typeof value
-  if (PRIMITIVES.has(type)) return 1
-  if (type !== "object") return 0
-  const tag = value[Symbol.toStringTag] || toString.call(value).slice(8, -1)
-  if (CLONEABLE.has(tag)) return 1
-  if (TRANSFERABLES.has(tag)) return 2
-  if ("toJSON" in value) return 3
-  return 0
+export function isSerializable(val) {
+  if (!val) return true
+  const type = typeof val
+  if (PRIMITIVES.has(type)) return true
+  if (type !== "object") return false
+  if (ArrayBuffer.isView(val) || isInstanceOf(val, Error)) return true
+  const tag = val[Symbol.toStringTag] || val.constructor?.name
+  return SERIALIZABLES.has(tag)
 }
+
+export default isSerializable
--- src/42/fabric/type/any/is/isTransferable.js
+++ src/42/fabric/type/any/is/isTransferable.js
@@ -1,6 +1,24 @@
-import isSerializable from "./isSerializable.js"
+// @src https://developer.mozilla.org/en-US/docs/Glossary/Transferable_objects#supported_objects
+export const TRANSFERABLES = new Set([
+  "ArrayBuffer",
+  "MessagePort",
+  "ReadableStream",
+  "WritableStream",
+  "TransformStream",
+  "WebTransportReceiveStream",
+  "WebTransportSendStream",
+  "AudioData",
+  "ImageBitmap",
+  "VideoFrame",
+  "OffscreenCanvas",
+  "RTCDataChannel",
+])
 
-export default function isTransferable(value) {
-  const res = isSerializable(value)
-  return res === 2 || value?.constructor?.name === "ArrayBuffer"
+export function isTransferable(val) {
+  if (!val) return false
+  if (typeof val !== "object") return false
+  const tag = val[Symbol.toStringTag] || val.constructor?.name
+  return TRANSFERABLES.has(tag)
 }
+
+export default isTransferable
